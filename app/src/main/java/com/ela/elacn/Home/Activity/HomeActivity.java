64b6b9ddf6abc5558ba9c077eccf55e981c0a914
zhangsjacob <zhangsjacob@gmail.com>
2019-08-12 04:12:10 -0400

    fixed UI bugs

--- app/src/main/java/com/ela/elacn/Home/Activity/HomeActivity.java
+++ app/src/main/java/com/ela/elacn/Home/Activity/HomeActivity.java
@@ -89,9 +89,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             int id = item.getItemId();
 
             //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
 
             return super.onOptionsItemSelected(item);
         }
